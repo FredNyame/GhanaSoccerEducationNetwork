a23a44605ded8afceacba940505c97c25bc50931
fredNyame <frednyame63@gmail.com>
2019-01-27 20:13:20 -0500

    Content added

--- js/nav.js
+++ js/nav.js
@@ -6,6 +6,9 @@
   //Get the element
   const menuItem = document.querySelector('.mobile-container');
   const dateElm = document.querySelector('#date');
+  let colMain = document.querySelector('.col-row');
+  colMain.addEventListener('click', serviceDesc, true);
+
   //Use Js to change the year for the footer Copyright
   dateElm.innerHTML = new Date().getFullYear();
   //li items 
@@ -17,5 +20,26 @@
     menuItem.classList.toggle('change');
     document.querySelector('.main_nav').classList.toggle('right-show');
     menuItems.forEach(item => item.classList.toggle('right-show'));
+  }
+
+  //function to show sevice info
+  function serviceDesc(e){
+    //console.log(e.target);
+    let rowCol  = colMain.querySelectorAll('.col-3');
+    let rowLen = rowCol.length;
+    //loop through them to add an event
+    for (let i = 0; i < rowLen; i++) {
+       let learnTags = rowCol[i].querySelectorAll('.strong');
+       let learnLen = learnTags.length;
+       for (let j = 0; j < learnLen; j++) {
+         let descTag = learnTags[j].parentNode.parentNode.nextElementSibling;
+         if(e.target === learnTags[j]){
+           descTag.classList.add('show');
+           console.log('show');
+         } else{
+          descTag.classList.remove('show');
+         }
+       }
+     }
   }
 })();
--- js/show.js
+++ js/show.js
@@ -1,22 +1,19 @@
-//Event on parrent
-let wrap = document.querySelector('.wrap');wrap.addEventListener('click', showDesc, true);
+(function () {
+  //Event on parrent
+  let wrap = document.querySelector('.wrap');
+  wrap.addEventListener('click', showDesc, true);
 
-function showDesc(e){
-  e.preventDefault();
-  let allImages = wrap.querySelectorAll('img');
-  for(let i = 0; i < allImages.length; i++){
-    let desc = allImages[i].parentElement.parentElement.nextElementSibling;
-    if(e.target === allImages[i]){
-      desc.classList.add('show');
-    } else{
-      desc.classList.remove('show');
+  //function to show the description
+  function showDesc(e){
+    e.preventDefault();
+    let allImages = wrap.querySelectorAll('img');
+    for(let i = 0; i < allImages.length; i++){
+      let desc = allImages[i].parentElement.parentElement.nextElementSibling;
+      if(e.target === allImages[i]){
+        desc.classList.add('show');
+      } else{
+        desc.classList.remove('show');
+      }
     }
-  }
-  //console.log(e.target);
-/*   if(e.target.tagName === 'IMG'){
-
-    desc.classList.add('show');
-  } else{
-    desc.classList.remove('show');
-  } */
 }
+})();
